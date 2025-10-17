fc252d5d12856be0266d463814cbabb30e711d2b
umair.gititall <rashid.summit2@gmail.com>
2025-10-18 01:36:40 +0500

    - FrontEnd Designing

--- app/src/main/java/com/overlord/ios/calculator/MainActivity.java
+++ app/src/main/java/com/overlord/ios/calculator/MainActivity.java
@@ -1,24 +1,50 @@
 package com.overlord.ios.calculator;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton fnc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        fnc = findViewById(R.id.functions);
+
+        fnc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, fnc);
+                popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(item -> {
+                    int id = item.getItemId();
+
+                    if(id == R.id.basic) {
+                        Toast.makeText(MainActivity.this, "Basic", Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+                    return false;
+                });
+                popup.show();
+            }
         });
     }
 }
